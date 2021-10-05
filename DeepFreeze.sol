3e1b73e76f066a04746325e0f44977c28877622d
Carlos R. Mercado <35497842+CR-Mercado@users.noreply.github.com>
2021-10-05 13:55:36 -0400

    Added request to include block-lock
    Owner can choose to lock the withdraw function for until N blocks after launch of freezer.

--- DeepFreeze.sol
+++ DeepFreeze.sol
@@ -6,7 +6,7 @@ contract CreateFreezer {
     
   address public creatorOwner;  // public state variable automatically has getter function 
   DeepFreeze[] public deployedFreezer; // public array automatically has getter function 
-  
+
   constructor(){ 
    creatorOwner = msg.sender; 
    // owner of the creator contract is the original deployer (i.e., protocol admin, not freezer owner)   
@@ -25,18 +25,33 @@ contract DeepFreeze {
      address payable public owner; // publicly visible owner of the freezer  
      string internal hint; // only this contract can see this.
      bytes32 internal answer; // only this contract can see this. 
-    
+     uint public launchblock; // block the freezer was locked at.
+     uint public blocklock; // optional, lock the withdraw function for a certain number of blocks. 
+
+
      constructor(address eoa, string memory hint_, bytes32 answer_){  // see createFreezer
       owner = payable(eoa); //  owner is freezer creator as payable
        hint = hint_; 
       answer = answer_;
+      launchblock = block.number;
+      blocklock = 0; // default to no lock
      }
      
      modifier onlyOwner() { 
        require(msg.sender == owner, "Only the freezer owner can do that!");
        _; 
      } 
+
+     modifier checkLock() { 
+       require(block.number > launchblock + blocklock, "This contract is still time-locked!");
+       _;
+
+     }
      
+    function LockWithdraw(uint numblocks) public onlyOwner { 
+      blocklock = numblocks;
+    }
+
      function requestHint() public onlyOwner view returns(string memory) { 
          return(hint);
      }
@@ -53,7 +68,7 @@ contract DeepFreeze {
       return address(this).balance;    
      } 
      
-     function withdraw(string memory passphrase_) public onlyOwner { 
+     function withdraw(string memory passphrase_) public onlyOwner checkLock { 
          require(  keccak256(abi.encodePacked(passphrase_)) == answer, "Your passphrase is wrong.");
          require (getBalance() != 0, "There's nothing to withdraw.");
          // Input code for withdrawing a specific ERC-20 asset.
